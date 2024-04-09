5f6b7f5521d35cf6340ce2e1a57dccea66d8b9be
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-09 18:36:59 -0300

    Corrigindo o nome de usuário

--- conexao_post.py
+++ conexao_post.py
@@ -2,7 +2,7 @@ import psycopg2
 
 conn = psycopg2.connect(
     database = 'fliperama',
-    user = 'root',
+    user = 'postgres',
     password = 'root',
     host = 'localhost',
     port = '5432',
--- lendo_dados_post.py
+++ lendo_dados_post.py
@@ -1,3 +1,13 @@
 from conexao_post import conn
 
+# Criando o cursor
 cursor_obj = conn.cursor()
+
+# Realizando a query
+cursor_obj.execute("SELECT *  FROM GAME")
+
+# Listar todos os registros
+result = cursor_obj.fetchall()
+
+# Printar os resultados
+print(result)
