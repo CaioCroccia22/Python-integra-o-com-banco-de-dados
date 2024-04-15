4cd5cdf58ab7439ecc244dfc3470497d5b38d6c5
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-15 18:37:32 -0300

    Criando o arquivo blog com as funções show_menu, add_user e add_post

---
+++ env/blog.py
@@ -0,0 +1,39 @@
+from conexao_ORM import Base, engine, session
+from User import User
+from Post import Post
+
+# Criando tabelas
+Base.metadata.create_all(engine)
+
+
+# Função para exibir o menu de opções 
+def show_menu():
+    print("Menu de opções:")
+    print("1. Adicionar Usuário")
+    print("2. Adicionar Post")
+    print("3. Consultar usuários")
+    print("4. Sair")
+
+def add_user():
+    print("Adicionar novo usuário:\n")
+    name = input("Digite o nome de usuário: \n")
+    email = input("Digite o email:\n")
+    user = User(name, email)
+    session.add(user)
+    session.commit()
+    print("Usuário adicionado com sucesso!!")
+
+# Função para adicionar novo Post
+    def add_Post():
+        print = ("Adicionar novo post:\n")
+        title = input("Digite o titulo do post: \n")
+        content = input("Digite o conteúdo do post: \n")
+        author_id = input("Digite o ID do autor: \n")
+        # Para adicionar o post tem que ver se o id do autor(chave estrageira) existe
+        # Para isso a variavel user
+        user = session.query(User).filter_by(id=author_id).first()
+        if user:
+            post = Post(title, content, author_id)
+            session.add(post)
+            session.commit()
+            print("Post adicionado com sucesso!")
