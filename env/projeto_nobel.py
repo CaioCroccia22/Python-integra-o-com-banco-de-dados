ce02af97a849b7f75ef60b37054f9cdb415d4269
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-22 20:33:44 -0300

    Instalação do módulo request e requisição API

--- env/projeto_nobel.py
+++ env/projeto_nobel.py
@@ -5,4 +5,17 @@ from pymongo import MongoClient
 # 1 - Estabelece conexão com o MongoDB
 client = MongoClient()
 
+
+# 2 - Criando o database
 db = client['nobel']
+
+# 3 - Importar dados em Documentos
+for collection_name in ["prizes", "laureates"]:
+    response = requests.get(
+        # Requisição da API
+        f"http://api.nobelprize.org/v1/{collection_name[:-1]}.json")
+        
+    documents = response.json()[collection_name]
+    print(documents)
+    
+
