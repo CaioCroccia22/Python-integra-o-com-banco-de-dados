867704b57c5368ee75a8f1a4722c50e0838fca03
caiocroccia22 <caiocroccia22@gmail.com>
2024-04-23 18:24:17 -0300

    Acessando a collections e fazendo a contagem de documentos

--- env/projeto_nobel.py
+++ env/projeto_nobel.py
@@ -16,6 +16,17 @@ for collection_name in ["prizes", "laureates"]:
         f"http://api.nobelprize.org/v1/{collection_name[:-1]}.json")
         
     documents = response.json()[collection_name]
-    print(documents)
+    db[collection_name].insert_many(documents)
+    
     
 
+# 4 - Acessando coleções / Contagem de documentos na coleção
+prizes = db["prizes"]
+laureates = db["laureates"]
+
+len_prizes = prizes.count_documents({})
+len_laureates = laureates.count_documents({})
+
+
+print(len_prizes)
+print(len_laureates)
